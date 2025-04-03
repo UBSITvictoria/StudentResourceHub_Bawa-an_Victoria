dfd472748bea6386820f144370aba179189c1daa
marc gil bawa-an <20234231@s.ubaguio.edu>
2025-04-03 15:25:23 +0800

    added components

---
+++ src/app/components/resource-list/resource-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-list',
+  imports: [],
+  templateUrl: './resource-list.component.html',
+  styleUrl: './resource-list.component.css'
+})
+export class ResourceListComponent {
+
+}
