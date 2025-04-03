4b8c53703dc0505129f8fe47623a9a98e19f17f8
marcGil20234231 <20234231@s.uabguio.edu>
2025-04-03 21:20:59 +0800

    created components

---
+++ src/app/components/add-resource-form/add-resource-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-resource-form',
+  imports: [],
+  templateUrl: './add-resource-form.component.html',
+  styleUrl: './add-resource-form.component.css'
+})
+export class AddResourceFormComponent {
+
+}
---
+++ src/app/components/resource-category/resource-category.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-category',
+  imports: [],
+  templateUrl: './resource-category.component.html',
+  styleUrl: './resource-category.component.css'
+})
+export class ResourceCategoryComponent {
+
+}
