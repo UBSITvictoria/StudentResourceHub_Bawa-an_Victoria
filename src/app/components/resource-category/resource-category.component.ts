3f49a9137118ffd81246bb82944dc136a734ca24
marc gil bawa-an <20234231@s.ubaguio.edu>
2025-04-03 15:46:49 +0800

    updated resource-category/details

--- src/app/components/resource-category/resource-category.component.ts
+++ src/app/components/resource-category/resource-category.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Resource } from '../../models/resource.model';
+import { ResourceService } from '../../services/resource.service';
+import { ResourceListComponent } from '../resource-list/resource-list.component';
 
 @Component({
   selector: 'app-resource-category',
-  imports: [],
-  templateUrl: './resource-category.component.html',
-  styleUrl: './resource-category.component.css'
+  imports: [ResourceListComponent],
+  template: `
+    <div class="container">
+      <h2>{{ category }} Resources</h2>
+      <p>Browse resources related to {{ category }}</p>
+      <app-resource-list [resources]="resources"></app-resource-list>
+    </div>
+  `,
+  styles: []
 })
-export class ResourceCategoryComponent {
+export class ResourceCategoryComponent implements OnInit {
+  category: string = '';
+  resources: Resource[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private resourceService: ResourceService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.category = params['category'];
+      this.resourceService.getResourcesByCategory(this.category).subscribe(resources => {
+        this.resources = resources;
+      });
+    });
+  }
 }
--- src/app/components/resource-details/resource-details.component.ts
+++ src/app/components/resource-details/resource-details.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Resource } from '../../models/resource.model';
+import { ResourceService } from '../../services/resource.service';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resource-details',
-  imports: [],
-  templateUrl: './resource-details.component.html',
-  styleUrl: './resource-details.component.css'
+  imports: [BrowserModule],
+  template: `
+    <div class="container" *ngIf="resource">
+      <div class="card">
+        <div class="card-header">
+          <h2>{{ resource.title }}</h2>
+        </div>
+        <div class="card-body">
+          <p class="card-text">{{ resource.description }}</p>
+          <div class="mb-3">
+            <strong>Category:</strong> {{ resource.category }}
+          </div>
+          <div class="mb-3">
+            <strong>URL:</strong> <a [href]="resource.url" target="_blank">{{ resource.url }}</a>
+          </div>
+          <div class="mb-3">
+            <strong>Author:</strong> {{ resource.author }}
+          </div>
+          <div class="mb-3">
+            <strong>Publication Date:</strong> {{ resource.publicationDate | date }}
+          </div>
+          <button class="btn btn-secondary" routerLink="/home">Back to List</button>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: []
 })
-export class ResourceDetailsComponent {
+export class ResourceDetailsComponent implements OnInit {
+  resource: Resource | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private resourceService: ResourceService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = +params['id'];
+      this.resource = this.resourceService.getResourceById(id);
+    });
+  }
 }
