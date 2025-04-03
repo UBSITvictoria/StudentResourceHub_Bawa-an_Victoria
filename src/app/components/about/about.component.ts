cc5f134aa6a370a333ede31e9ab67de22072b01a
marc gil bawa-an <20234231@s.ubaguio.edu>
2025-04-03 15:51:00 +0800

    updated about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  template: `
+    <div class="container">
+      <h2>About Student Resource Hub</h2>
+      <p>The Student Resource Hub is an application designed to provide educational resources for students, organized by category.</p>
+      <p>This application was created as part of the APPDEV1 course.</p>
+    </div>
+  `,
+  styles: []
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
