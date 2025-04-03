2ea78394e838c253da8ac4e0bf13775be9165e55
marcGil20234231 <20234231@s.uabguio.edu>
2025-04-03 21:33:09 +0800

    updated navigations

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -2,10 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" routerLink="/home" routerLinkActive="active">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown">
+                Categories
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" routerLink="/categories/Programming">Programming</a></li>
+                <li><a class="dropdown-item" routerLink="/categories/Design">Design</a></li>
+                <li><a class="dropdown-item" routerLink="/categories/Math">Math</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" routerLink="/about" routerLinkActive="active">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" routerLink="/add-resource" routerLinkActive="active">Add Resource</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  `,
+  styles: []
 })
-export class NavigationComponent {
-
-}
+export class NavigationComponent {}
